cbfa84eb830b4d492cf9e4a7dd5de7d489f21ef9
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-10-15 18:59:14 +0300

    basic output on different lines

---
+++ basic-output/basicoutput.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Strathmore University is the leading" << std::endl;
+    std::cout << "university in the region" << std::endl;
+    return 0;
+}
